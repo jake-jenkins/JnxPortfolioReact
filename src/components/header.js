8d03c83e08d2a2e4f818cb4e477ee413d6d7b091
Jake Jenkins <dev@jakejnx.com>
2021-01-29 01:32:10 +0000

    spacing

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const Header = ({ siteTitle }) => (
     <img class="logo" src="https://cdn.spdy.live/uploads/jnx2_d83e25e919.svg"></img>
     </a>
   </div>
-  <div class="flex-1 text-center">
+  <div class="flex-1 text-center space-x-2">
     <a href="/"><button class="py-2 px-4 font-semibold text-white bg-blue-800 hover:bg-blue-600">Home</button></a>
     <a href="/about"><button class="py-2 px-4 font-semibold text-white bg-blue-800 hover:bg-blue-600">About</button></a>
     <a href="/portfolio"><button class="py-2 px-4 font-semibold text-white bg-blue-800 hover:bg-blue-600">Portfolio</button></a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ const IndexPage = () => {
   return (
 <Layout>
 <h1>Hello</h1>
+<p>Sorry this site is currently being rebuilt. I hope to have it live by 01/02/21.</p>
 </Layout>
   )
 }
